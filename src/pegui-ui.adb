b1e521db71ef279e4abc366e14cfd1405c0eb614
LTC-117 <lucastentoni@gmail.com>
2025-11-22 17:44:55 -0300

    [Implementing basic body for compilation test]

--- src/pegui-ui.adb
+++ src/pegui-ui.adb
@@ -2,4 +2,9 @@ package body Pegui.UI is
 
    -- TODO : Finish implementing specifications file first!
 
+   function Test_Pegui_UI return Boolean is
+   begin
+      return True;
+   end Test_Pegui_UI;
+
 end Pegui.UI;
