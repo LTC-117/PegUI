e3ddd7119e9e8e29dc0bd29955762000f3ea55f2
LTC-117 <lucastentoni@gmail.com>
2025-11-22 17:43:28 -0300

    [Implementing basic body for compile test]

--- src/pegui-wrapper.adb
+++ src/pegui-wrapper.adb
@@ -2,4 +2,9 @@ package body Pegui.Wrapper is
 
    -- TODO : Finish implementing specifications file first!
 
+   function Check_FFmpeg return Boolean is
+   begin
+      return True;
+   end Check_FFmpeg;
+
 end Pegui.Wrapper;
