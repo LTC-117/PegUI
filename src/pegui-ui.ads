d82182db40a87ecdbc6aa0f1a65df11e324e2258
LTC-117 <lucastentoni@gmail.com>
2025-11-21 22:29:28 -0300

    [Brief description of Pegui.UI package implementation]

---
+++ src/pegui-ui.ads
@@ -0,0 +1,7 @@
+package Pegui.UI is
+
+   -- This package implements an abstraction layer for the GUI of the app.
+   -- It's gonna be responsible for all of the window functionalities,
+   -- elements and characteristics. Depends on Pegui.Widgets and Pegui.Wrapper.
+
+end Pegui.UI;
