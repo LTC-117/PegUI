fcee6f40a4a86a1419fc02139579e2054c6e0b91
LTC-117 <lucastentoni@gmail.com>
2025-11-21 22:30:13 -0300

    [Brief description of Pegui.Widgets package implementation]

---
+++ src/pegui-widgets.ads
@@ -0,0 +1,10 @@
+package Pegui.Widgets is
+
+   -- This package implements types and fundamental functionalities for the
+   -- elements of the application's window. It's purpose is simple : abstract
+   -- most of the native gtk elements implementation, aiming to standardize them
+   -- through the application. For example : Create standards for buttons and 
+   -- text sizes, formats, fonts, colors and positions. It should standardize
+   -- video and audio rendering types and functionalities as well.
+
+end Pegui.Widgets;
