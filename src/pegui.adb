a34d5baeabc2074315ce0a1837f76c1b559a38df
LTC-117 <lucastentoni@gmail.com>
2025-11-21 22:13:50 -0300

    [Changing window name (PegUI)]

--- src/pegui.adb
+++ src/pegui.adb
@@ -5,14 +5,15 @@ with Glib;        use Glib;
 procedure Pegui is
 
    Window   :  Gtk_Window;
-   W_Width  :  Gint := 800;
-   W_Height :  Gint := 600;
+   W_Width  :  constant Gint := 800;
+   W_Height :  constant Gint := 600;
 
 begin
 
    Gtk.Main.Init;
 
    Gtk.Window.Gtk_New (Window);
+   Gtk.Window.Set_Title (Window, "PegUI");
 
    Set_Default_Size (Window => Window, Width => W_Width, Height => W_Height);
 
