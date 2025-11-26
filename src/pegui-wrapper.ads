0b3a2ffd09798a64e27caa63ba14be96f6923240
LTC-117 <lucastentoni@gmail.com>
2025-11-25 23:18:20 -0300

    [Removing unnecessary comments + applying comments conventions]

--- src/pegui-wrapper.ads
+++ src/pegui-wrapper.ads
@@ -3,28 +3,17 @@ with GNAT.OS_Lib;       use GNAT.OS_Lib;
 
 package Pegui.Wrapper is
 
-   -- This package implements a simple yet complete and fully functional wrapper
-   -- for the ffmpeg media converter. It must offer a simple abstraction and
-   -- interaction layer for the other modules of the project to successfuly
-   -- interact with the ffmpeg executable through shell commands. The entire
-   -- project depends on this package.
+   --  This package implements a simple yet complete and fully functional wrapper
+   --  for the ffmpeg media converter. It must offer a simple abstraction and
+   --  interaction layer for the other modules of the project to successfuly
+   --  interact with the ffmpeg executable through shell commands. The entire
+   --  project depends on this package.
 
+   FFmpeg_Command_Count :  constant Positive := 10;
    FFmpeg : constant String := "ffmpeg";
 
-   --type FFmpeg_Commands is record
-      -- Fill in with all possible ffmpeg commands
-      -- All of them must be type 'constant Argument_List'. E.g.:
-      --Start_Recording : Argument_List; -- := "argument list for cmd"
-      -- This is but an example. Full functional implementation soon.
-      -- ...
-   --end record;
 
    function Check_FFmpeg return Boolean;
-   -- Checks if the machine running the application has FFmpeg installed.
-
-   --function Run_FFmpeg_Command (Command : FFmpeg_Commands) return Boolean;
-   -- Runs respective FFmpeg argument passed. Must be one of the arguments
-   -- listed inside 'FFmpeg_Commands' type. Returns True only if command
-   -- executes successfuly.
+   --  Checks if the machine running the application has FFmpeg installed.
 
 end Pegui.Wrapper;
