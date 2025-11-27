99db83c8f306376f0ccdcaa3685e41e60c7ac34e
LTC-117 <lucastentoni@gmail.com>
2025-11-26 23:40:07 -0300

    [Basic test implementation of Widgets_Render_Button_Bar function]

--- src/peg-widgets.adb
+++ src/peg-widgets.adb
@@ -7,4 +7,9 @@ package body Peg.Widgets is
       return True;
    end Test_Pegui_Widgets;
 
+   function Widgets_Render_Button_Bar return Widget_Button_Bar_Response is
+   begin
+      return Options;
+   end Widgets_Render_Button_Bar;
+
 end Peg.Widgets;
