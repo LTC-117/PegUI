7499fa77b9bde6512e1001bd9a6b84fafb91dcfa
LTC-117 <lucastentoni@gmail.com>
2025-11-26 23:24:48 -0300

    [Uploading package description]

--- src/peg-ui.ads
+++ src/peg-ui.ads
@@ -1,9 +1,11 @@
 package Peg.UI is
 
    --  This package implements an abstraction layer for the GUI of the app.
-   --  It's gonna be responsible for all of the window functionalities,
-   --  elements and characteristics. Depends on Pegui.Widgets and Pegui.Wrapper.
+   --  It must be responsible for all of the window functionalities,
+   --  elements and characteristics, uniting all the elements from the Peg-UI
+   --  package, with the functionalities of Peg.Wrapper package.
 
    function Test_Pegui_UI return Boolean;
+   --  Merely here to test if the package would work. Remove soon.
 
 end Peg.UI;
