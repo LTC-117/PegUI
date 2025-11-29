b0918b2c4d548c2da5644955d3c7513a96c2f7a5
LTC-117 <lucastentoni@gmail.com>
2025-11-28 22:20:07 -0300

    [Simple test implementation for the button bar (merely a test, must be reimplemented soon)]

--- src/peg-widgets.adb
+++ src/peg-widgets.adb
@@ -1,15 +1,59 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Gtk.Grid;    use Gtk.Grid;
+with Gtk.Button;  use Gtk.Button;
+with Glib.Object; use Glib.Object;
+
+with Gtk.Enums;
+
 package body Peg.Widgets is
 
    --  TODO : Finish implementing specifications file first!
 
-   function Test_Pegui_Widgets return Boolean is
-   begin
-      return True;
-   end Test_Pegui_Widgets;
+   -------------------------------
+   -- Widgets_Render_Button_Bar --
+   -------------------------------
 
-   function Widgets_Render_Button_Bar return Widget_Button_Bar_Response is
+   function Widgets_Render_Button_Bar return Gtk_Grid is
+
+      Button_Bar     : Gtk_Grid;
+      Button_Options : Gtk_Button;
+      Button_Configs : Gtk_Button;
+      Button_Help    : Gtk_Button;
+      Button_Buffer  : GObject;
+
    begin
-      return Options;
+
+      Button_Bar := Gtk.Grid.Gtk_Grid_New;
+
+      Button_Options := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Options");
+      Button_Configs := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Configs");
+      Button_Help    := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Help");
+
+      Gtk.Grid.Attach
+         (Self   => Button_Bar,
+          Child  => Button_Options,
+          Left   => 0,
+          Top    => 0,
+          Width  => 5,
+          Height => 1);
+
+      Gtk.Grid.Attach
+         (Self   => Button_Bar,
+          Child  => Button_Configs,
+          Left   => 6,
+          Top    => 0,
+          Width  => 5,
+          Height => 1);
+
+      Gtk.Grid.Attach
+         (Self   => Button_Bar,
+          Child  => Button_Help,
+          Left   => 11,
+          Top    => 0,
+          Width  => 5,
+          Height => 1);
+
+      return Button_Bar;
    end Widgets_Render_Button_Bar;
 
 end Peg.Widgets;
