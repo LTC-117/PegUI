442c50cc8f917786c27c939959e3e348d2375f55
LTC-117 <lucastentoni@gmail.com>
2025-11-28 22:13:05 -0300

    [Simple comment (style motivated change). Change not important in any way]

--- src/peg-ui.adb
+++ src/peg-ui.adb
@@ -2,6 +2,10 @@ package body Peg.UI is
 
    --  TODO : Finish implementing specifications file first!
 
+   -------------------
+   -- Test_Pegui_UI --
+   -------------------
+
    function Test_Pegui_UI return Boolean is
    begin
       return True;
