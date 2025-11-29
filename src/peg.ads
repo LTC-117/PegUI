bbd231c910f480feda3aaddcfcd028e341f3e33a
LTC-117 <lucastentoni@gmail.com>
2025-11-28 22:18:10 -0300

    [Introducing 2 more main global variables (Main_Box and Main_Grid) + minor comment changes]

--- src/peg.ads
+++ src/peg.ads
@@ -1,11 +1,12 @@
 with Gtk.Main;
 with Gtk.Application;
 
-with Gtk.Label;    use Gtk.Label;
-with Gtk.Window;   use Gtk.Window;
-with Gtk.Widget;   use Gtk.Widget;
-with Gtk.Table;    use Gtk.Table;
-with Glib;         use Glib;
+with Gtk.Window;  use Gtk.Window;
+with Gtk.Widget;  use Gtk.Widget;
+with Gtk.Grid;    use Gtk.Grid;
+with Gtk.Box;     use Gtk.Box;
+with Gtk.Label;   use Gtk.Label;
+with Glib;        use Glib;
 
 with Ada.Unchecked_Conversion;
 with Gdk.Event;
@@ -14,21 +15,29 @@ package Peg is
 
    --  Overall, this package must implement basic types, sizes, functions and
    --  any other thing that could be considered fundamental and basic for the
-   --  application. For now, the functions must be focused in the window.
+   --  application, which means anything that should be funcamental to the main
+   --  state machine. For now, the functions must be focused in the window.
 
-   ---------------------
-   --  Application types
+   -----------------------
+   -- Application types --
+   -----------------------
 
-   --------------------------------------
-   --  Application fundamental variables.
+   ---------------------------------------
+   -- Application fundamental variables --
+   ---------------------------------------
 
    Window_Height : constant Gint  := 500;
    Window_Width  : constant Gint  := 800;
-   Main_Window   : Gtk_Window;
-   Button        : Gtk_Widget;
+   --  Window size;
 
-   -----------------------------------------------
-   --  Application basic procedures and functions.
+   Main_Window : Gtk_Window;
+   Main_Box    : Gtk_VBox;
+   Main_Grid   : Gtk_Grid;
+   -- Main application hierarchy : Window -> Box -> Grid
+
+   ------------------------------------------------
+   -- Application basic procedures and functions --
+   ------------------------------------------------
 
    procedure Pegui_Begin;
    --  First part of window initialization. Responsible for initializing the
