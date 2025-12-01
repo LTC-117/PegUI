278ee7fe42a5c8f55dd1abb1e1a1c47e0ed0b54a
LTC-117 <lucastentoni@gmail.com>
2025-12-01 00:09:19 -0300

    [Basic minimal demonstration of the state machine structure]

--- src/pegui.adb
+++ src/pegui.adb
@@ -4,9 +4,17 @@ with Peg.Widgets;  use Peg.Widgets;
 with Peg.Wrapper;  use Peg.Wrapper;
 
 procedure Pegui is
+
+   Pegui_State : UI_Screen_State := Peg.UI.Main;
+
 begin
    Pegui_Begin;
 
+      case Pegui_State is
+         when Main  => Pegui_State := Peg.UI.UI_Main_Screen;
+         when Video => Pegui_State := Peg.UI.UI_Video_Screen;
+         when Audio => Pegui_State := Peg.UI.UI_Audio_Screen;
+      end case;
 
    Pegui_End;
 end Pegui;
