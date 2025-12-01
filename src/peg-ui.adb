94f41518bd12226650656e28afc58c2b9fadb967
LTC-117 <lucastentoni@gmail.com>
2025-12-01 00:10:57 -0300

    [Implementing basic demonstrative state functions (merely a demontration, must be deeply implemented later)]

--- src/peg-ui.adb
+++ src/peg-ui.adb
@@ -1,14 +1,68 @@
+with Gtk.Button;  use Gtk.Button;
+with Peg;   use Peg;
+
 package body Peg.UI is
 
    --  TODO : Finish implementing specifications file first!
 
-   -------------------
-   -- Test_Pegui_UI --
-   -------------------
+   --------------------
+   -- UI_Main_Screen --
+   --------------------
 
-   function Test_Pegui_UI return Boolean is
+   function UI_Main_Screen return UI_Screen_State is
+      Button : Gtk_Button;
    begin
-      return True;
-   end Test_Pegui_UI;
+      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Main");
+
+      Gtk.Grid.Attach
+         (Self   => Peg.Main_Grid,
+          Child  => Button,
+          Left   => 0,
+          Top    => -10,
+          Width  => 50,
+          Height => 1);
+
+      return Main;
+   end UI_Main_Screen;
+
+   ---------------------
+   -- UI_Video_Screen --
+   ---------------------
+
+   function UI_Video_Screen return UI_Screen_State is
+      Button : Gtk_Button;
+   begin
+      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Video");
+
+      Gtk.Grid.Attach
+         (Self   => Peg.Main_Grid,
+          Child  => Button,
+          Left   => 0,
+          Top    => -10,
+          Width  => 50,
+          Height => 1);
+
+      return Video;
+   end UI_Video_Screen;
+
+   ---------------------
+   -- UI_Audio_Screen --
+   ---------------------
+
+   function UI_Audio_Screen return UI_Screen_State is
+      Button : Gtk_Button;
+   begin
+      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Audio");
+
+      Gtk.Grid.Attach
+         (Self   => Peg.Main_Grid,
+          Child  => Button,
+          Left   => 0,
+          Top    => -10,
+          Width  => 50,
+          Height => 1);
+
+      return Audio;
+   end UI_Audio_Screen;
 
 end Peg.UI;
