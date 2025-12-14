5eb5aae64d8029b2ac090b9034ba6bee5ccfbe41
LTC-117 <lucastentoni@gmail.com>
2025-12-13 21:25:58 -0300

    [Minor changes]

--- src/peg-ui.adb
+++ src/peg-ui.adb
@@ -1,6 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
 with Gtk.Button;  use Gtk.Button;
 with Gtk.Grid;    use Gtk.Grid;
-
 with Peg;         use Peg;
 
 package body Peg.UI is
@@ -12,19 +12,19 @@ package body Peg.UI is
    --------------------
 
    function UI_Main_Screen return UI_Screen_State is
-      Button : Gtk_Button;
+      Btn : Gtk_Button;
    begin
-      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Main");
-      Set_Halign (Button, Align_Center);
-      Set_Valign (Button, Align_Center);
+      Btn := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Main");
+      Set_Halign (Btn, Align_Center);
+      Set_Valign (Btn, Align_Center);
 
       Gtk.Grid.Attach
          (Self   => Peg.Main_Grid,
-          Child  => Button,
+          Child  => Btn,
           Left   => 0,
           Top    => 0,
-          Width  => 15,
-          Height => 15);
+          Width  => 5,
+          Height => 5);
 
       return Main;
    end UI_Main_Screen;
@@ -34,17 +34,19 @@ package body Peg.UI is
    ---------------------
 
    function UI_Video_Screen return UI_Screen_State is
-      Button : Gtk_Button;
+      Btn : Gtk_Button;
    begin
-      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Video");
+      Btn := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Video");
+      Set_Halign (Btn, Align_Center);
+      Set_Valign (Btn, Align_Center);
 
       Gtk.Grid.Attach
          (Self   => Peg.Main_Grid,
-          Child  => Button,
+          Child  => Btn,
           Left   => 0,
-          Top    => -10,
-          Width  => 50,
-          Height => 1);
+          Top    => 0,
+          Width  => 5,
+          Height => 5);
 
       return Video;
    end UI_Video_Screen;
@@ -54,17 +56,19 @@ package body Peg.UI is
    ---------------------
 
    function UI_Audio_Screen return UI_Screen_State is
-      Button : Gtk_Button;
+      Btn : Gtk_Button;
    begin
-      Button := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Audio");
+      Btn := Gtk.Button.Gtk_Button_New_With_Mnemonic ("Audio");
+      Set_Halign (Btn, Align_Center);
+      Set_Valign (Btn, Align_Center);
 
       Gtk.Grid.Attach
          (Self   => Peg.Main_Grid,
-          Child  => Button,
+          Child  => Btn,
           Left   => 0,
-          Top    => -10,
-          Width  => 50,
-          Height => 1);
+          Top    => 0,
+          Width  => 5,
+          Height => 5);
 
       return Audio;
    end UI_Audio_Screen;
