8361778ecff57548f3487411b2a9e8521be63d6f
LTC-117 <lucastentoni@gmail.com>
2025-12-29 16:23:36 -0300

    [Testing Pegui FSM (it works)]

--- src/pegui.adb
+++ src/pegui.adb
@@ -4,17 +4,12 @@ with Peg.Widgets;  use Peg.Widgets;
 with Peg.Wrapper;  use Peg.Wrapper;
 
 procedure Pegui is
-
-   Pegui_State : UI_Screen_State := Peg.UI.Main;
-
 begin
+   -- Peg.UI.UI_Pegui_State := Peg.UI.Main;
+
    Pegui_Begin;
 
-      case Pegui_State is
-         when Main  => Pegui_State := Peg.UI.UI_Main_Screen;
-         when Video => Pegui_State := Peg.UI.UI_Video_Screen;
-         when Audio => Pegui_State := Peg.UI.UI_Audio_Screen;
-      end case;
+      Peg.UI.Render_UI;
 
    Pegui_End;
 end Pegui;
